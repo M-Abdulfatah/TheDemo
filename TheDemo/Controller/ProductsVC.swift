ecadfbafd719464eff980926e2b3eee29a18fc83
Mahmoud Mohammed <progmahmoud95@gmail.com>
2019-05-19 14:52:26 +0200

    Rename ViewController to ProductsVC

--- TheDemo/Controller/ProductsVC.swift
+++ TheDemo/Controller/ProductsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProductsVC.swift
 //  TheDemo
 //
 //  Created by Mahmoud Mohammed on 5/18/19.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProductsVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-
 }
-
